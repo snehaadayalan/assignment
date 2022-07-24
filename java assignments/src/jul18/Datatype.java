e38154c0186af7c77d4beac271ed42f1949acd94
Sneha <sakthid099@gmail.com>
2022-07-24 19:14:44 +0530

    first commit

---
+++ java assignments/src/jul18/Datatype.java
@@ -0,0 +1,25 @@
+package jul18;
+
+public class Datatype {
+
+		public static void main(String[] args) {
+			
+			String name = "sneha";
+			int age=21;
+			float f=3.45f;
+			long l=67459930l;
+			double d=4444444.7777777;
+			char gender = 'f';
+		
+			
+			System.out.println("My name is"+" "+ name);
+			System.out.println("My char value is"+" "+ gender);
+			System.out.println("My age is"+" "+age);
+			System.out.println("My float value is"+" "+ f);
+			System.out.println("My long value is"+" "+ l);
+			System.out.println("My double value is"+" "+ d);
+			
+
+		}
+
+	}
